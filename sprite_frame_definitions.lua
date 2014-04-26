825d34e6fcd76692361898c8f1221eeec9a32cce
Chris Saunders <chris.saunders@shopify.com>
2014-04-26 21:47:04 +0000

    Add example sprite frame definition

---
+++ sprite_frame_definitions.lua
@@ -0,0 +1,14 @@
+SpriteFrameDefinitions = {}
+SpriteFrameDefinitions.Princess1 = {
+  'idle' = {
+    {
+      'image' = 'Assets/Art/pixel anims/p2-idle-1.png',
+      'duration' = '2.0',
+    },
+    {
+      'image' = 'Assets/Art/pixel anims/p2-idle-2.png',
+      'duration' = '1.0',
+      'event' = 'fire'
+    }
+  }
+}
