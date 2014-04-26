ede3f01908c6d4099a6d0c4c0fd4c08008bcec5d
david rusak <davidrusak@gmail.com>
2014-04-26 18:10:08 -0400

    first-pass values for anim timings

--- sprite_frame_definitions.lua
+++ sprite_frame_definitions.lua
@@ -3,12 +3,50 @@ SpriteFrameDefinitions.Princess1 = {
   'idle' = {
     {
       'image' = 'Assets/Art/pixel anims/p2-idle-1.png',
-      'duration' = '2.0',
+      'duration' = 0.5,
     },
     {
       'image' = 'Assets/Art/pixel anims/p2-idle-2.png',
-      'duration' = '1.0',
-      'event' = 'fire'
+      'duration' = 0.2,
+    },
+    {
+      'image' = 'Assets/Art/pixel anims/p2-idle-3.png',
+      'duration' = 0.5,
+    },
+    {
+      'image' = 'Assets/Art/pixel anims/p2-idle-4.png',
+      'duration' = 0.2,
+      'next' = 1,
+    }
+  }
+'shooting' = {
+    {
+      'image' = 'Assets/Art/pixel anims/p2-fire-1.png',
+      'duration' = 0.3,
+    },
+    {
+      'image' = 'Assets/Art/pixel anims/p2-fire-2.png',
+      'duration' = 0.1,
+      'event' = 'fire',
+    },
+    {
+      'image' = 'Assets/Art/pixel anims/p2-fire-3.png',
+      'duration' = 0.5,
+    }
+  }
+'dodging' = {
+    {
+      'image' = 'Assets/Art/pixel anims/p2-dodge-1.png',
+      'duration' = 0.15,
+    },
+    {
+      'image' = 'Assets/Art/pixel anims/p2-dodge-2.png',
+      'duration' = 0.3,
+      'event' = 'dodge',
+    },
+    {
+      'image' = 'Assets/Art/pixel anims/p2-dodge-3.png',
+      'duration' = 0.15,
     }
   }
 }
