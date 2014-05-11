fc0b9f6022416ca073eee799d101642c14d3c73b
Chris Saunders <chris.saunders@shopify.com>
2014-05-11 00:25:53 +0000

    adding shootout scene in preparation for migration over to gamestates

---
+++ scenes/shootout.lua
@@ -0,0 +1,40 @@
+Shootout = {}
+
+function Shootout:init()
+end
+
+function Shootout:enter(previous, ...)
+end
+
+function Shootout:leave()
+end
+
+function Shootout:update(dt)
+end
+
+function Shootout:draw()
+end
+
+function Shootout:focus(isfocused)
+end
+
+function Shootout:keypressed(key, isrepeat)
+end
+
+function Shootout:keyreleased(key)
+end
+
+function Shootout:mousepressed(x, y, button)
+end
+
+function Shootout:mousereleased(x, y, button)
+end
+
+function Shootout:joystickpressed(joystick, button)
+end
+
+function Shootout:joystickreleased(joystick, button)
+end
+
+function Shootout:quit()
+end
